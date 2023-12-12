67e1394cab8dc2750a6383f3fb438b694cb73b59
mechztcc <dojathan@gmail.com>
2023-12-12 09:30:13 -0500

    feat: card team member

--- src/app/modules/teams/components/card-team-member/card-team-member.component.ts
+++ src/app/modules/teams/components/card-team-member/card-team-member.component.ts
@@ -1,12 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-card-team-member',
   standalone: true,
   imports: [],
   templateUrl: './card-team-member.component.html',
-  styleUrl: './card-team-member.component.scss'
+  styleUrl: './card-team-member.component.scss',
 })
 export class CardTeamMemberComponent {
-
+  @Input()
+  member: { name: string; id: number; score: number };
 }
